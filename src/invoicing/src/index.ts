78ab7e05d79856b3de1bd95ece34f86afb05392d
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-08-04 09:42:57 +0300

    asd

--- src/invoicing/src/index.ts
+++ src/invoicing/src/index.ts
@@ -3,13 +3,9 @@ import { InvoiceCreated } from '../../domain/invoice-created.js'
 import { configure } from './config.js'
 import { onConnect, startRabbitMQ, consumeAny, publish } from './eventBus.js'
 
-// Service identity — the only line that should need changing when this
-// service is copied as a template for a new one. Also sets the dex
-// client_id and the expected `<SERVICE_NAME>_CLIENT_SECRET` env var.
-// Must run before eventBus/auth do anything, since they read it lazily.
 configure('invoicing')
 
-console.log('invoicing service starting')
+console.log('service starting')
 
 onConnect(() => {
   void consumeAny(CreateInvoiceRequested, (request, requestedBy) => {
@@ -19,5 +15,4 @@ onConnect(() => {
 })
 
 startRabbitMQ()
-
-console.log('invoicing service started')
+console.log('service started')
