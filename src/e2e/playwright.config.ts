6e443d45e089d39f366790c4ce4546db8b118508
Alin-Florin Ciu <alinflorinciu@icloud.com>
2026-08-14 11:23:13 +0300

    asd

--- src/e2e/playwright.config.ts
+++ src/e2e/playwright.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
   reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'html',
 
   use: {
